dfb015083a4e924370b0d71f4b3339ac57dc3f84
bascher <bascher.dev@gmail.com>
2024-02-29 10:19:59 +0000

    added Dockerfiles for microservices

--- infrastructure/msp-5-dev-server-of-petclinic/petclinicserver-userdata.sh
+++ infrastructure/msp-5-dev-server-of-petclinic/petclinicserver-userdata.sh
@@ -5,9 +5,9 @@ dnf install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
-curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 dnf install git -y
 dnf install java-11-amazon-corretto -y
-cd /home/ec2-user && git clone https://github.com/baaserosman/petclinic-microservices-with-db.git
+cd /home/ec2-user && git clone https://github.com/bascher/petclinic-microservices-with-db.git
 git checkout dev
